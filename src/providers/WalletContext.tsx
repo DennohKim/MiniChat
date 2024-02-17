ea7cf077ae0d38407de44a377de36000445a9940
Dennis Kimathi <Kimathidennis38@gmail.com>
2024-02-17 08:46:10 +0300

    fix wallet context

--- src/providers/WalletContext.tsx
+++ src/providers/WalletContext.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import { createContext, useEffect, useState } from 'react';
 import Web3Modal from 'web3modal';
@@ -6,8 +6,8 @@ import { ethers } from 'ethers';
 
 // Define the shape of the context
 interface WalletContextType {
-//   connectWallet: () => Promise<void>;
-//   disconnectWallet: () => void;
+  connectWallet: () => Promise<void>;
+  disconnectWallet: () => void;
   walletAddress: string | null;
   signer: ethers.providers.JsonRpcSigner | null;
   selectedConvo: any; // Replace 'any' with the actual type of 'selectedConvo'
@@ -17,8 +17,8 @@ interface WalletContextType {
 }
 
 export const WalletContext = createContext<WalletContextType>({
-//   connectWallet: async () => {},
-//   disconnectWallet: () => {},
+  connectWallet: async () => {},
+  disconnectWallet: () => {},
   walletAddress: null,
   signer: null,
   selectedConvo: null, // or a default value if it's not nullable
@@ -27,8 +27,11 @@ export const WalletContext = createContext<WalletContextType>({
   setLinkToSend: () => {},
 });
 
-
-export const WalletContextProvider = ({ children }: {children: React.ReactNode}) => {
+export const WalletContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
   const [walletAddress, setWalletAddress] = useState<string | null>(null);
   const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner | null>(
     null
@@ -36,77 +39,67 @@ export const WalletContextProvider = ({ children }: {children: React.ReactNode})
   const [selectedConvo, setSelectedConvo] = useState(null);
   const [linkToSend, setLinkToSend] = useState();
 
+  const connectMiniPay = async () => {
+    if (
+      typeof window !== 'undefined' &&
+      window.ethereum &&
+      window.ethereum.isMiniPay
+    ) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      setSigner(signer); // setSigner expects a JsonRpcSigner or null
+      const address = await signer.getAddress(); // getAddress returns a Promise<string>
+      setWalletAddress(address); // setWalletAddress expects a string or null
+    } else {
+      console.error('MiniPay provider not detected');
+    }
+  };
 
-//   // Provider options for Web3Modal
-//   const providerOptions = {
-//     // Additional provider options go here
-//   };
+  // Call connectMiniPay when the component mounts
+  useEffect(() => {
+    connectMiniPay();
+  }, []);
 
-//   // Instantiate Web3Modal with the provider options
-//   const web3Modal = new Web3Modal({
-//     cacheProvider: true,
-//     providerOptions,
-//   });
+  // Function to connect to the modal
+  const connectWallet = async () => {
+    if (typeof window !== 'undefined') {
+      try {
+        const instance = await new Web3Modal({
+          cacheProvider: true,
+        }).connect();
+        const web3Provider = new ethers.providers.Web3Provider(instance, 'any');
+        const newSigner = await web3Provider.getSigner();
+        setSigner(newSigner);
+        const newAddress = await newSigner.getAddress();
+        setWalletAddress(newAddress);
 
-//   // Function to disconnect wallet
-//   const disconnectWallet = () => {
-//     if (typeof window !== 'undefined') {
-//       setWalletAddress(null);
-//       setSigner(null);
-//     }
-//   };
-
-//   // Function to connect to the modal
-//   const connectWallet = async () => {
-//     try {
-//       const instance = await web3Modal.connect();
-//       const web3Provider = new ethers.providers.Web3Provider(instance, 'any');
-//       const newSigner = await web3Provider.getSigner();
-//       setSigner(newSigner);
-//       const newAddress = await newSigner.getAddress();
-//       setWalletAddress(newAddress);
-
-//       // Event listeners for account change, connect, and disconnect
-//       instance.on('accountsChanged', () => {
-//         disconnectWallet();
-//       });
-//       instance.on('connect', () => {
-//         connectWallet();
-//       });
-//       instance.on('disconnect', () => {
-//         disconnectWallet();
-//       });
-//     } catch (err) {
-//       console.error('Failed to connect wallet: ', err);
-//     }
-//   };
-
-    const connectMiniPay = async () => {
-      if (
-        typeof window !== 'undefined' &&
-        window.ethereum &&
-        window.ethereum.isMiniPay
-      ) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        setSigner(signer); // setSigner expects a JsonRpcSigner or null
-        const address = await signer.getAddress(); // getAddress returns a Promise<string>
-        setWalletAddress(address); // setWalletAddress expects a string or null
-      } else {
-        console.error('MiniPay provider not detected');
+        // Event listeners for account change, connect, and disconnect
+        instance.on('accountsChanged', () => {
+          disconnectWallet();
+        });
+        instance.on('connect', () => {
+          connectWallet();
+        });
+        instance.on('disconnect', () => {
+          disconnectWallet();
+        });
+      } catch (err) {
+        console.error('Failed to connect wallet: ', err);
       }
-    };
+    }
+  };
 
-    // Call connectMiniPay when the component mounts
-    useEffect(() => {
-      connectMiniPay();
-    }, []);
+  // Function to disconnect wallet
+  const disconnectWallet = () => {
+    setWalletAddress(null);
+    setSigner(null);
+  };
 
   return (
     <WalletContext.Provider
       value={{
-        // connectWallet,
-        // disconnectWallet,
+        connectWallet,
+        disconnectWallet,
         walletAddress,
         signer,
         selectedConvo,
